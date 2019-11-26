98a31a293aa9cead70da1e7c9c4a3bbd63859d92
mhervy <mhervy@eni.fr>
2019-11-26 12:01:26 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,15 @@ export class AppComponent  {
     const el = this.contentImg.nativeElement;
     // Seulement testable une fois publier car seulement possible sur mobile
     if (event.scale > 1) {
-      el.style.transform = 'scale( 1 * ' + event.scale + ')';
+      el.style.transform = 'scale(' +  (1 * + event.scale) + ')';
     }
+  }
+
+  test() {
+    const el = this.contentImg.nativeElement;
+    // Seulement testable une fois publier car seulement possible sur mobile
+    
+      el.style.transform = 'scale(' + (1 * + 1.2 ) + ')';
+    
   }
 }
